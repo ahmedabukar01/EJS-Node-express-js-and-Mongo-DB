99f0e7698f8632903d81f490e9e7a24131c540b0
ahmed <ahmedlearn01@gmail.com>
2021-12-19 12:27:49 -0800

    on

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// middleware
+app.set('view engine', 'ejs');
+app.use(express.static('styles'));
+
+// const dbUrl = 'mongodb+srv://ahmed:ahmed123@mymango.oifxe.mongodb.net/blogs?retryWrites=true&w=majority';
+
+// mongoose.connect(dbUrl)
+// .then(result=>{
+//     app.listen('3000', ()=>{
+//         console.log('we are a live now!');
+//     });
+// })
+// .catch(err=>console.log(err));
 
 app.listen('3000', ()=>{
     console.log('we are a live now!');
 });
 
 app.get('/',(req,res)=>{
-    res.send('<h1> hey how are you doing ?</h1>')
+    res.render('index', {title: 'All Blogs'});
 })
